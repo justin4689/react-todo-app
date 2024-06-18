b36eb65a0764ab40f422b43e889f07c44a368812
justin4689 <justintrah8@gmail.com>
2024-06-18 16:41:02 +0000

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactPages from 'vite-plugin-react-pages';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+    ,
+    reactPages()
+
+  ],
 })
