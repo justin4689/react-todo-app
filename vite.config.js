7488627b03af393eb8a6232b37b1ee66720eaef5
justin4689 <justintrah8@gmail.com>
2024-06-18 17:02:36 +0000

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reactPages from 'vite-plugin-react-pages';
-
+import reactPages from 'vite-plugin-react-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-todo-app/', // Remplacez par le nom de votre repository
   plugins: [
-    react()
-    ,
+    react(),
     reactPages()
-
   ],
 })
